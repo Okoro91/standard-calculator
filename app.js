2b178ca9b77db09bf912815d12c0710de39d3958
okoro <mi.fracri91@gmail.com>
2023-11-29 07:20:33 +0100

    operator function created

--- app.js
+++ app.js
@@ -4,3 +4,23 @@
  let divide = (a, b) => a / b;
  let modulos = (a, b) => a % b;
  
+ let firstDigit;
+ let seconDigit;
+ let assignment;
+
+ let operate = function(num1, num2, assign){
+
+    if(assign === add){
+       return add(num1, num2);
+    }else if(assign === subtract){
+       return subtract(num1, num2);
+    }else if(assign === multiply){
+       return multiply(num1, num2);
+    }else if(assign === divide){
+       return divide(num1, num2);
+    }else if(assign === modulos){
+       return modulos(num1, num2);
+    }
+ }
+
+ console.log(operate(10, 8, modulos));
